6db52aa836fa049e654792e08ec48a4b472ac1bd
s32951 <s32951@pjwstk.edu.pl>
2025-05-29 17:46:45 +0200

    Add files via upload
    Zadanie 1

---
+++ Vehicle.java
@@ -0,0 +1,10 @@
+public class Vehicle {
+    public void start() {
+        System.out.println("Pojazd uruchomiony");
+    }
+
+    public void stop() {
+        System.out.println("Pojazd zatrzymany");
+    }
+}
+
